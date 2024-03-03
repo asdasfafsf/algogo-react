77a395b32783f9ddb85251741b749511b6d55a7c
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-03-04 02:02:11 +0900

    feat: add 404

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,15 +2,20 @@ import {
     createBrowserRouter
 } from 'react-router-dom';
 import App from './App';
+import NotFound from '../template/NotFound';
 
 
 
 const router = createBrowserRouter([
     {
-      path: "/",
-      element: <App />,
+        path: "/",
+        element: <App />,
     },
-]);
+    {
+        path: '/*',
+        element: <NotFound />
+    }
+].sort(a => a.path === '/*' ? -1 : 1));
 
 
 export default router;
