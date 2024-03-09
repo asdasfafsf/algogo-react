fed4ed969d9f4bf5acef8d8c07b230af6220c0bd
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-03-09 23:40:42 +0900

    feat: 라인 파라미터를 유동적으로 조절 가능하도록 함

--- src/atom/Line.tsx
+++ src/atom/Line.tsx
@@ -1,3 +1,9 @@
-export default function Line() {
-  return <div className="h-px w-screen bg-gray-200" />;
+interface LineProps {
+  // eslint-disable-next-line react/require-default-props
+  className?: string;
+}
+
+export default function Line({ className }: LineProps) {
+  const defaultClassName = `${className ?? ''} h-px w-full bg-gray-300`.trim();
+  return <div className={defaultClassName} />;
 }
