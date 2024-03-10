771bb6fc5fd34abc99bf7c3baf8438a6b2b0278f
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-03-10 18:30:46 +0900

    etc: initialize Problem Level UI

---
+++ src/atom/ProblemLevelChip.tsx
@@ -0,0 +1,13 @@
+import { Chip } from '@material-tailwind/react';
+
+export default function ProblemLevelChip() {
+  return (
+    <Chip
+      variant="ghost"
+      className="text-blue-800"
+    //   color="#624637"
+      size="sm"
+      value="브론즈 5"
+    />
+  );
+}
