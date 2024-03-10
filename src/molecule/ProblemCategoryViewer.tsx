8488a8419e7cac034707789ae22a9bfb2d0aad4b
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-03-10 18:22:45 +0900

    feat: tip 위치 미세조정

--- src/molecule/ProblemCategoryViewer.tsx
+++ src/molecule/ProblemCategoryViewer.tsx
@@ -15,12 +15,12 @@ export default function ProblemCategoryViewer(
   const [categoryState, tooltipContent, handleClick] = useProblemCategoryViewer(initialState);
 
   return (
+    <Tooltip
+      content={tooltipContent}
+      placement="top-start"
+    >
+      <div className="min-h-12 my-4 max-w-[calc(100%-48px)] ">
 
-    <div className="min-h-12 my-4 max-w-[calc(100%-48px)] ">
-      <Tooltip
-        content={tooltipContent}
-        placement="top"
-      >
         <div className="lex items-center flex-wrap max-w-[calc(100%-48px)] ">
           {/* <Typography variant="small" className="font-bold mx-2 h-full w-8">유형 </Typography> */}
 
@@ -35,8 +35,8 @@ export default function ProblemCategoryViewer(
                 : categoryList.map((category) => <ProblemCategoryChip category={category} />)}
           </div>
         </div>
-      </Tooltip>
-    </div>
+      </div>
+    </Tooltip>
 
   );
 }
