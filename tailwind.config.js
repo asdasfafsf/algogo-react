24d9e0cde542da8da4dd1c8ba6d959d8091ee844
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-03-10 22:44:41 +0900

    feat:티어 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,26 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 export default withMT({
   content: ["./src/**/*.{html,js,ts,tsx,jsx}"],
   theme: {
+    colors: {
+      ruby: '#ff0062',
+      diamond: '#01b5fc',
+      platinum: '#26e2a5',
+      gold: '#ec9a00',
+      silver: '#425e7a',
+      bronze: '#ad5701',
+      // bronze: {
+      //   default: '#ad5701',
+      //   100: '#ffe6cc',
+      //   200: '#ffd9b3',
+      //   300: '#fecc9a',
+      //   400: '#febf80',
+      //   500: '#feb367',
+      //   600: '#fea64d',
+      //   700: '#fe8c1b',
+      //   800: '#e47301',
+      //   900: '#ad5701',
+      // },
+    },
     extend: {},
   },
   plugins: [],
