3d2fd14d9c74dade0db1144824c21fa6d08e01df
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-03-12 00:25:35 +0900

    etc: 드래그 진행중

--- src/organism/ProblemSidebar.tsx
+++ src/organism/ProblemSidebar.tsx
@@ -8,110 +8,171 @@ import ClipboardWithTooltip from '../atom/ClipboardWithTooltip';
 import ProblemImage from '../atom/ProblemImage';
 import ProblemCategoryViewer from '../molecule/ProblemCategoryViewer';
 import ProblemLevelViewer from '../molecule/ProblemLevelViewer';
+import {
+  useCallback, useEffect, useRef, useState,
+} from 'react';
 
 export default function ProblemSidebar() {
-  const sample = '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1\n1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1\n1 1 1 1 1 1 1 1 1 1 1 1\n끝';
+  const sample = `1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1
+끝`;
   const categoryList: ProblemCategory[] = ['구현', '그래프 이론', '다이나믹 프로그래밍'];
+  const draggableRef = useRef<HTMLDivElement>(null);
+  const [size, setSize] = useState(500);
+  const handleMouseDown = useCallback((clickEvent: React.MouseEvent<Element, MouseEvent>) => {
+    clickEvent.stopPropagation();
+    let currentSize = size;
+    const screenWidth = (window.innerWidth
+      || document.documentElement.clientWidth
+      || document.body.clientWidth);
+
+    console.log(`start SX ${clickEvent.screenX}`);
+    const mouseMoveHandler = (moveEvent: MouseEvent) => {
+      // 4️⃣
+      console.log('start\n\n');
+      console.log(`size : ${size}`);
+      console.log(`currentSize : ${currentSize}`);
+      const deltaX = moveEvent.screenX - (currentSize + 40);
+      console.log(`deltaX : ${deltaX}`);
+      console.log(`sX ${moveEvent.screenX}`);
+      // setSize(clickEvent.screenX)
+      if ((deltaX > 0 && (deltaX + currentSize) < screenWidth - 100)
+        || (deltaX < 0 && (deltaX + currentSize) > 100)) {
+        currentSize += deltaX;
+        setSize(currentSize);
+        console.log(`newSize : ${currentSize}`);
+      }
+      console.log('end\n\n');
+    };
+
+    // 5️⃣
+    const mouseUpHandler = () => {
+      document.removeEventListener('mousemove', mouseMoveHandler);
+    };
+
+    // 1️⃣
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler, { once: true });
+  }, [size]);
+
   return (
     <aside
-      className="px-5 py-8 overflow-y-auto w-[500px]"
+      style={{
+        width: `${size}px`,
+      }}
+      className="relative flex"
     >
-      <Typography variant="h4">포물선의 방정식</Typography>
-      <Line className="my-2" />
+      <div className="px-5 py-8 overflow-y-auto w-full">
+        <Typography variant="h4">포물선의 방정식</Typography>
+        <Line className="my-2" />
 
-      <div className="h-4 my-2">
-        <div className="flex flex-wrap items-center gap-4 jus">
-          <ProblemLevelViewer intialState="hide" level="알 수 없음" />
-          <div className="flex flex-wrap items-center">
-            <Typography variant="small" className="font-bold">제출 : </Typography>
+        <div className="h-4 my-2">
+          <div className="flex flex-wrap items-center gap-4 jus">
+            <ProblemLevelViewer intialState="hide" level="알 수 없음" />
+            <div className="flex flex-wrap items-center">
+              <Typography variant="small" className="font-bold">제출 : </Typography>
               &nbsp;
-            <Typography variant="small" className="font-medium">6554231</Typography>
-          </div>
-          <div className="flex flex-wrap items-center">
-            <Typography variant="small" className="font-bold">정답률 : </Typography>
+              <Typography variant="small" className="font-medium">6554231</Typography>
+            </div>
+            <div className="flex flex-wrap items-center">
+              <Typography variant="small" className="font-bold">정답률 : </Typography>
             &nbsp;
-            <Typography variant="small" className="font-medium">33.12%</Typography>
+              <Typography variant="small" className="font-medium">33.12%</Typography>
+            </div>
           </div>
         </div>
-      </div>
 
-      <ProblemCategoryViewer initialState={categoryList.length === 0 ? 'none' : 'hide'} categoryList={categoryList} />
+        <ProblemCategoryViewer initialState={categoryList.length === 0 ? 'none' : 'hide'} categoryList={categoryList} />
 
-      <Typography variant="paragraph" className="font-normal">
-        포물선의 방정식을 구할 지 물어보고
-      </Typography>
-      <Typography variant="paragraph" className="font-normal">
-        예를 선택한다면 초점을 입력받고 방적식을 출력하게 한다.
-      </Typography>
-      <Typography variant="paragraph" className="font-normal">
-        아니오를 선택 시 프로그램을 종료한다는 문구를 출력한다
-      </Typography>
+        <Typography variant="paragraph" className="font-normal">
+          포물선의 방정식을 구할 지 물어보고
+        </Typography>
+        <Typography variant="paragraph" className="font-normal">
+          예를 선택한다면 초점을 입력받고 방적식을 출력하게 한다.
+        </Typography>
+        <Typography variant="paragraph" className="font-normal">
+          아니오를 선택 시 프로그램을 종료한다는 문구를 출력한다
+        </Typography>
 
-      <ProblemImage src="https://onlinejudgeimages.s3-ap-northeast-1.amazonaws.com/upload/201003/dfcmhrjj_142c3w76qg8_b.jpg" alt="테스트1" />
+        <ProblemImage src="https://onlinejudgeimages.s3-ap-northeast-1.amazonaws.com/upload/201003/dfcmhrjj_142c3w76qg8_b.jpg" alt="테스트1" />
 
-      <Typography variant="paragraph" className="font-normal">
-        위와 같을 경우에는 아무렇게나 출력한다 아몰랑ㅇㅇㅇㅇㅇ 길게쓰면 어떻게되나요??아몰랑ㅇㅇㅇㅇㅇ 길게쓰면 어떻게되나요??아몰랑ㅇㅇㅇㅇㅇ 길게쓰면 어떻게되나요??아몰랑ㅇㅇㅇㅇㅇ 길게쓰면 어떻게되나요??아몰랑ㅇㅇㅇㅇㅇ 길게쓰면 어떻게되나요??아몰랑ㅇㅇㅇㅇㅇ 길게쓰면 어떻게되나요??
-      </Typography>
+        <Typography variant="paragraph" className="font-normal">
+          위와 같을 경우에는 아무렇게나 출력한다 아몰랑ㅇㅇㅇㅇㅇ
+          길게쓰면 어떻게되나요??아몰랑ㅇㅇㅇㅇㅇ 길게쓰면 어떻게되나요??
+          아몰랑ㅇㅇㅇㅇㅇ 길게쓰면 어떻게되나요??아몰랑ㅇㅇㅇㅇㅇ 길게쓰면
+          어떻게되나요??아몰랑ㅇㅇㅇㅇㅇ 길게쓰면 어떻게되나요??아몰랑ㅇㅇㅇㅇㅇ
+          길게쓰면 어떻게되나요??
+        </Typography>
 
-      <Line className="my-4 opacity-0" />
-      <Typography variant="h5">입력</Typography>
-      <Line className="mt-2 mb-4" />
+        <Line className="my-4 opacity-0" />
+        <Typography variant="h5">입력</Typography>
+        <Line className="mt-2 mb-4" />
 
-      <Typography variant="paragraph" className="font-normal">
-        입력은 요러케 저러케 조로케 한다
-      </Typography>
+        <Typography variant="paragraph" className="font-normal">
+          입력은 요러케 저러케 조로케 한다
+        </Typography>
 
-      <Line className="my-4 opacity-0" />
-      <Typography variant="h5">출력</Typography>
-      <Line className="mt-2 mb-4" />
+        <Line className="my-4 opacity-0" />
+        <Typography variant="h5">출력</Typography>
+        <Line className="mt-2 mb-4" />
 
-      <Typography variant="paragraph" className="font-normal">
-        출력도 요로케 저러케 이렇게 한다
-      </Typography>
+        <Typography variant="paragraph" className="font-normal">
+          출력도 요로케 저러케 이렇게 한다
+        </Typography>
 
-      <Line className="my-4 opacity-0" />
-      <Typography variant="h5">입출력 예시</Typography>
-      <Line className="mt-2 mb-4" />
+        <Line className="my-4 opacity-0" />
+        <Typography variant="h5">입출력 예시</Typography>
+        <Line className="mt-2 mb-4" />
 
-      <div className="flex items-center gap-4">
-        <div className="flex justify-center items-center">
-          <div className="flex justify-center items-center bg-black rounded-sm text-blue-500 h-6 w-6">
-            <EnterIcon />
-          </div>
+        <div className="flex items-center gap-4">
+          <div className="flex justify-center items-center">
+            <div className="flex justify-center items-center bg-black rounded-sm text-blue-500 h-6 w-6">
+              <EnterIcon />
+            </div>
               &nbsp;
-          <Typography variant="small" className="font-medium">: 다음 줄</Typography>
-        </div>
-        <div className="flex justify-center items-center">
-          <div className="flex justify-center items-center bg-black rounded-sm text-blue-500 h-6 w-6">
-            <SpaceIcon />
+            <Typography variant="small" className="font-medium">: 다음 줄</Typography>
           </div>
+          <div className="flex justify-center items-center">
+            <div className="flex justify-center items-center bg-black rounded-sm text-blue-500 h-6 w-6">
+              <SpaceIcon />
+            </div>
               &nbsp;
-          <Typography variant="small" className="font-medium">: 스페이스</Typography>
+            <Typography variant="small" className="font-medium">: 스페이스</Typography>
+          </div>
         </div>
+        <Line className="my-4 opacity-0" />
+
+        <Typography variant="h6" className="font-bold pt-2">예시1</Typography>
+        <Typography variant="small" className="font-medium">입력</Typography>
+        <ClipboardWithTooltip content="1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5" />
+        <Typography variant="small" className="font-medium mt-1">출력</Typography>
+        <ClipboardWithTooltip content={sample} />
+
+        <Typography variant="h6" className="font-bold pt-2">예시2</Typography>
+        <Typography variant="small" className="font-medium">입력</Typography>
+        <ClipboardWithTooltip content="1 2 3 4" />
+        <Typography variant="small" className="font-medium mt-1">출력</Typography>
+        <ClipboardWithTooltip content="1 2 3 4" />
+
+        <Typography variant="h6" className="font-bold pt-2">예시3</Typography>
+        <Typography variant="small" className="font-medium">입력</Typography>
+        <ClipboardWithTooltip content="1 2 3 4" />
+        <Typography variant="small" className="font-medium mt-1">출력</Typography>
+        <ClipboardWithTooltip content="1 2 3 4" />
+
+        <Line className="my-4 opacity-0" />
+        <Typography variant="h5">출처</Typography>
+        <Line className="my-2" />
       </div>
-      <Line className="my-4 opacity-0" />
-
-      <Typography variant="h6" className="font-bold pt-2">예시1</Typography>
-      <Typography variant="small" className="font-medium">입력</Typography>
-      <ClipboardWithTooltip content="1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5" />
-      <Typography variant="small" className="font-medium mt-1">출력</Typography>
-      <ClipboardWithTooltip content={sample} />
-
-      <Typography variant="h6" className="font-bold pt-2">예시2</Typography>
-      <Typography variant="small" className="font-medium">입력</Typography>
-      <ClipboardWithTooltip content="1 2 3 4" />
-      <Typography variant="small" className="font-medium mt-1">출력</Typography>
-      <ClipboardWithTooltip content="1 2 3 4" />
-
-      <Typography variant="h6" className="font-bold pt-2">예시3</Typography>
-      <Typography variant="small" className="font-medium">입력</Typography>
-      <ClipboardWithTooltip content="1 2 3 4" />
-      <Typography variant="small" className="font-medium mt-1">출력</Typography>
-      <ClipboardWithTooltip content="1 2 3 4" />
-
-      <Line className="my-4 opacity-0" />
-      <Typography variant="h5">출처</Typography>
-      <Line className="my-2" />
+      <div
+        ref={draggableRef}
+        onMouseDown={handleMouseDown}
+        className="z-10 h-[calc(100vh-48px)]  text-white -right-5 absolute w-5 bg-pink cursor-col-resize bg-pink"
+      >
+        드래그 영역이에용
+      </div>
     </aside>
   );
 }
