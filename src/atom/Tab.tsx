4fcfea8938843da1d168bfa380faa35467cfca9c
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-03-16 21:40:13 +0900

    feat: add tab className

--- src/atom/Tab.tsx
+++ src/atom/Tab.tsx
@@ -4,13 +4,16 @@ interface TabProps {
   text: string;
   isSelected: boolean;
   handleClick: (e: React.MouseEvent) => Promise<void> | void;
+  className: string
 }
 
-export default function Tab({ text, isSelected, handleClick }: TabProps) {
+export default function Tab({
+  text, isSelected, handleClick, className,
+}: TabProps) {
   return (
     <li
       onClick={handleClick}
-      className={`${isSelected ? 'bg-gray-900 text-white' : 'text-blue-gray-500'} text-blue-gray-500 cursor-pointer h-full min-w-12 p-4 flex items-center relative z-10 w-auto`}
+      className={`${isSelected ? 'bg-gray-900 text-white' : 'text-blue-gray-500'} ${className} text-blue-gray-500 cursor-pointer h-full min-w-12 p-4 flex items-center relative z-10 w-auto`}
     >
       {isSelected ? <div className="w-full bg-blue-500 h-1 absolute top-0 left-0" /> : ''}
       <Typography
