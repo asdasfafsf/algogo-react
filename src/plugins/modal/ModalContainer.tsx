8cc349a3dc1c2ba307a47320c6526d56e33ce8fc
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-03-21 20:20:28 +0900

    feat: add modal plugin

---
+++ src/plugins/modal/ModalContainer.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/jsx-no-useless-fragment */
+import { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import useModal from './useModal';
+
+const MODAL_ID = 'modal-container';
+export default function ModalContainer() {
+  const modal = useModal();
+  const topComponentInfo = modal.top();
+
+  useEffect(() => {
+    if (document.getElementById(MODAL_ID)) {
+      return;
+    }
+
+    const modalDOM = document.createElement('div');
+    modalDOM.id = MODAL_ID;
+    document.body.append(modalDOM);
+  }, []);
+
+  if (!topComponentInfo) {
+    return <></>;
+  }
+
+  return ReactDOM.createPortal(
+    <div>
+      <topComponentInfo.Component
+        resolve={topComponentInfo.resolve}
+        reject={topComponentInfo.reject}
+        {...(topComponentInfo?.props ?? {})}
+      />
+    </div>,
+    window.document.getElementById(MODAL_ID)!,
+
+  );
+}
---
+++ src/plugins/modal/ModalController.ts
@@ -0,0 +1,61 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Dispatch, SetStateAction } from 'react';
+
+interface ModalInfo {
+  key: string;
+  Component: React.FC<any>;
+  props: unknown;
+  resolve: (value: any) => void;
+  reject: (value: any) => void;
+}
+
+export default class ModalController {
+  private flagState: [number, Dispatch<SetStateAction<number>>];
+
+  private modalInfos: ModalInfo[] = [];
+
+  constructor(flagState: [number, Dispatch<SetStateAction<number>>]) {
+    this.flagState = flagState;
+  }
+
+  private flush() {
+    const setFlag = this.flagState[1];
+    setFlag((prev) => prev + 1);
+  }
+
+  top() {
+    return this.modalInfos[this.modalInfos.length - 1];
+  }
+
+  private handlePromises(key: string, resolver: (value: any) => void, value: any) {
+    resolver(value);
+    this.modalInfos = this.modalInfos.filter((elem) => key !== elem.key);
+    this.flush();
+  }
+
+  clear() {
+    while (this.modalInfos.length) {
+      this.pop();
+    }
+    this.flush();
+  }
+
+  pop() {
+    this.top()?.reject('Close modal');
+    this.modalInfos.pop();
+    this.flush();
+  }
+
+  async push(key: string, Component: React.FC<any>, props: unknown) {
+    return new Promise((resolve, reject) => {
+      this.modalInfos.push({
+        key,
+        Component,
+        props,
+        resolve: (value) => this.handlePromises(key, resolve, value),
+        reject: (reason) => this.handlePromises(key, reject, reason),
+      });
+      this.flush();
+    });
+  }
+}
