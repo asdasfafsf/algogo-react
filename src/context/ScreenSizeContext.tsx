e089b0474e66b85be4a77cf53a9099f33321a807
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-04-01 19:36:58 +0900

    feat: add ScreenSixeContext

---
+++ src/context/ScreenSizeContext.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+/* eslint-disable react/function-component-definition */
+import React, {
+  createContext, useState, useEffect, useContext,
+} from 'react';
+
+interface ScreenSizeContextType {
+  isMobile: boolean;
+}
+
+interface ScreenSizeContextProps {
+  children: React.ReactNode;
+}
+
+const ScreenSizeContext = createContext<ScreenSizeContextType | undefined>(undefined);
+
+export const ScreenSizeProvider = ({ children }: ScreenSizeContextProps) => {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // 예시: 768px 미만을 모바일로 간주
+    };
+
+    handleResize(); // 초기 실행
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <ScreenSizeContext.Provider value={{ isMobile }}>
+      {children}
+    </ScreenSizeContext.Provider>
+  );
+};
+
+export const useScreenSize = (): ScreenSizeContextType => {
+  const context = useContext(ScreenSizeContext);
+  if (context === undefined) {
+    throw new Error('useScreenSize must be used within a ScreenSizeProvider');
+  }
+  return context;
+};
