1bf37d332f83e2be761f194ecefd09ce7b37e28f
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-04-07 03:16:17 +0900

    fix: 탭 사이즈 정리

--- src/organism/CodeResultPannel.tsx
+++ src/organism/CodeResultPannel.tsx
@@ -4,15 +4,8 @@ import TabHeader from '../atom/TabHeader';
 import Tab from '../atom/Tab';
 import TabPanel from '../atom/TabPanel';
 import TabBody from '../atom/TabBody';
-import { useCodeEditorHeightStore } from '../zustand/CodeResultHeightStore';
 import { useExecuteResultListStore } from '../zustand/ExecuteResultListStore';
-import {
-  CODE_CONTROL_PANEL_HEIGHT,
-  EDITOR_REASIZE_AREA_HEIGHT,
-  PROBLEM_HEADER_HEIGHT,
-} from '../constant/Size';
 import CodeEditorResizer from '../molecule/CodeEditorResizer';
-import { useScreenSize } from '../context/ScreenSizeContext';
 import CodeResultInput from './CodeResultInput';
 import CodeResultOutput from './CodeResultOutput';
 import CodeTestCaseTable from './CodeTestCaseTable';
@@ -21,7 +14,6 @@ export default function CodeResultPannel() {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const inputTextAreaRef = useRef<HTMLTextAreaElement>(null);
   const outputTextAreaRef = useRef<HTMLTextAreaElement>(null);
-  const codeEditorHeight = useCodeEditorHeightStore((state) => state.codeEditorHeight);
   const executeResultList = useExecuteResultListStore((state) => state.executeResultList);
 
   return (
@@ -34,14 +26,14 @@ export default function CodeResultPannel() {
         <Tab text="실행 결과" isSelected={selectedIndex === 1} handleClick={() => { setSelectedIndex(1); }} />
         <Tab text="테스트 케이스" isSelected={selectedIndex === 2} handleClick={() => { setSelectedIndex(2); }} />
       </TabHeader>
-      <TabBody className="h-[calc(100%-40px)]">
+      <TabBody className="h-[calc(100%-44px)]">
         <TabPanel isSelected={selectedIndex === 0}>
-          <div>테스트</div>
-          {/* <CodeResultInput inputTextAreaRef={inputTextAreaRef} /> */}
+          {/* <div>테스트</div> */}
+          <CodeResultInput inputTextAreaRef={inputTextAreaRef} />
         </TabPanel>
         <TabPanel isSelected={selectedIndex === 1}>
-          <div>테스트</div>
-          {/* <CodeResultOutput outputTextAreaRef={outputTextAreaRef} /> */}
+          {/* <div>테스트</div> */}
+          <CodeResultOutput outputTextAreaRef={outputTextAreaRef} />
         </TabPanel>
         <TabPanel isSelected={selectedIndex === 2}>
           {/* <div>테스트</div> */}
