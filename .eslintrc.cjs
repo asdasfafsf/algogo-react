6b70e6f467a3f81d4195090ef9478af4cbe61457
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-05-11 15:03:15 +0900

    feat: 새 규칙 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,7 @@ module.exports = {
       "jsx-a11y/no-static-element-interactions": 'off',
       "@typescript-eslint/no-shadow": "off",
       "jsx-a11y/click-events-have-key-events": "off",
-      "jsx-a11y/no-noninteractive-element-interactions": "off"
+      "jsx-a11y/no-noninteractive-element-interactions": "off",
+      "import/no-named-as-default": "off"
   }
 }
