f6caf85da1738b0885062f711b3ed5670af179f6
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-05-11 15:07:33 +0900

    feat: 커스텀 훅으로 변경

--- src/organism/ProblemTypeDropdown.tsx
+++ src/organism/ProblemTypeDropdown.tsx
@@ -1,44 +1,39 @@
 import { Button, Typography } from '@material-tailwind/react';
 import Dropdown from '../atom/Dropdown';
 import ChipWithSelected from '../atom/ChipWithSelected';
+import useProbleTypeDropdown from '../hook/useProblemTypeDropdown';
 
-interface ProblemTypeDropdownProps {
-  optionList: ProblemOption[],
-  handleSelect: (e: React.MouseEvent, index: number) => void | Promise<void>
-  handleOk: (e: React.MouseEvent<HTMLButtonElement>) => void | Promise<void>
-  handleReset: (e: React.MouseEvent<HTMLButtonElement>) => void | Promise<void>
-}
+export default function ProblemTypeDropdown() {
+  const [open,
+    problemTypeList,
+    handleSelect,
+    handleReset,
+    handleOk,
+    handler] = useProbleTypeDropdown();
 
-export default function ProblemTypeDropdown({
-  optionList,
-  handleSelect,
-  handleOk,
-  handleReset,
-}: ProblemTypeDropdownProps) {
   return (
-    <Dropdown value="유형">
+    <Dropdown
+      handler={handler}
+      open={open}
+      value="유형"
+    >
       <div>
         <div className="mb-2">
           <Typography variant="small">유형 </Typography>
         </div>
         <div className="flex flex-wrap gap-2 max-w-80">
 
-          {optionList.map(({
-            isSelected, name, value, type,
-          }, index) => {
-            if (type === '유형') {
-              return (
-                <ChipWithSelected
-                  key={value}
-                  onClick={(e) => handleSelect(e, index)}
-                  size="sm"
-                  value={name}
-                  isSelected={isSelected}
-                />
-              );
-            }
-            return '';
-          })}
+          {problemTypeList.map(({
+            isSelected, name, value,
+          }, index) => (
+            <ChipWithSelected
+              key={value}
+              onClick={(e) => handleSelect(e, index)}
+              size="sm"
+              value={name}
+              isSelected={isSelected}
+            />
+          ))}
 
         </div>
         <div className="flex items-center justify-end gap-2 mt-4">
