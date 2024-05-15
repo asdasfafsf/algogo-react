4a39321d9945ef60160137fbd702bffb2baee2a1
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-05-15 17:47:12 +0900

    feat: 전체 필터에 적용되도록 추가

--- src/hook/useProblemStateDropdown.ts
+++ src/hook/useProblemStateDropdown.ts
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import useProblemTableFilterStore from '../zustand/ProblemTableFilterStore';
 
 export default function useProblemStateDropdown() {
   const [problemStateList, setProblemStateList] = useState([
@@ -6,6 +7,48 @@ export default function useProblemStateDropdown() {
     { isSelected: false, name: '맞힌 문제', value: '맞힌 문제' },
     { isSelected: false, name: '틀린 문제', value: '틀린 문제' },
   ]);
+
+  const setProblemOptionList = useProblemTableFilterStore((state) => state.setProblemOptionList);
+
+  useEffect(() => {
+    setProblemOptionList((prevList) => {
+      const newProblemOptionList = prevList.filter((problemOption) => {
+        if (problemOption.type !== '상태') {
+          return true;
+        }
+
+        const target = problemStateList.find(
+          (problemState) => problemState.name === problemOption.name,
+        );
+
+        if (!target) {
+          return false;
+        }
+
+        return target.isSelected;
+      });
+
+      problemStateList.forEach((problemState) => {
+        const target = newProblemOptionList.find(
+          (problemOption) => problemOption.name === problemState.name,
+        );
+
+        if (target) {
+          return true;
+        }
+
+        if (problemState.isSelected) {
+          newProblemOptionList.push({
+            type: '상태',
+            ...problemState,
+          });
+        }
+        return true;
+      });
+
+      return newProblemOptionList;
+    });
+  }, [problemStateList]);
 
   const handleClick = (e: React.MouseEvent, index: number) => {
     e.stopPropagation();
