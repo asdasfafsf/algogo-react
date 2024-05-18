578bb819fb308b815491a9a5397fdf7c71edbac4
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-05-18 13:25:27 +0900

    style: 색상 변경

--- src/atom/Dropdown.tsx
+++ src/atom/Dropdown.tsx
@@ -27,7 +27,7 @@ export default function Dropdown({
       <div
         ref={menuRef}
         onClick={handleOpen}
-        className={`${isOpen ? 'bg-blue-100' : 'bg-gray-300'} flex items-center h-4 gap-1 px-2 py-4 rounded-md cursor-pointer`}
+        className={`${isOpen ? 'bg-blue-100' : 'bg-gray-100'} flex items-center h-4 gap-1 px-2 py-4 rounded-md cursor-pointer`}
       >
         <Typography
           className={`${isOpen ? 'text-blue-500' : ''} font-bold`}
