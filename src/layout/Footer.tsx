7339e368a2fde103b0dc12a081c4761d9b31d4f1
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-05-23 20:51:14 +0900

    feat: 푸터 수정

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,39 +1,54 @@
-/* eslint-disable jsx-a11y/control-has-associated-label */
-import { Typography, IconButton } from '@material-tailwind/react';
+import { Typography } from '@material-tailwind/react';
 
 export default function Footer() {
   return (
-    <footer className="px-8 py-16">
+    <footer className="px-8 py-8">
       <div className="container mx-auto">
-        <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-          <Typography className="!text-sm font-medium text-gray-500 lg:text-left text-center">
-            All rights reserved. Copyright &copy;
-            {' '}
-            {new Date().getFullYear()}
+        <div className="flex items-center gap-24">
+          <Typography
+            className="relative top-1 font-WavvePADO-Regular"
+            variant="h3"
+          >
+            <a href="/">
+              알고고
+            </a>
           </Typography>
-          <div className="flex gap-2 lg:ml-auto place-content-center">
-            <a href="#buttons-with-link1">
-              <IconButton variant="text" size="sm">
-                <i className="text-lg text-gray-500 transition-colors fa-brands fa-twitter hover:text-blue-gray-900" />
-              </IconButton>
-            </a>
-            <a href="#buttons-with-link2">
-              <IconButton variant="text" size="sm">
-                <i className="text-lg text-gray-500 transition-colors fa-brands fa-youtube hover:text-blue-gray-900" />
-              </IconButton>
-            </a>
-            <a href="#buttons-with-link3">
-              <IconButton variant="text" size="sm">
-                <i className="text-lg text-gray-500 transition-colors fa-brands fa-instagram hover:text-blue-gray-900" />
-              </IconButton>
-            </a>
-            <a href="#buttons-with-link4">
-              <IconButton variant="text" size="sm">
-                <i className="text-lg text-gray-500 transition-colors fa-brands fa-github hover:text-blue-gray-900" />
-              </IconButton>
-            </a>
+
+          <div>
+            <div className="flex items-center gap-4">
+              <Typography
+                variant="small"
+              >
+                이용안내
+              </Typography>
+              <Typography
+                variant="small"
+              >
+                개인정보 처리방침
+              </Typography>
+            </div>
+            <div className="mt-2">
+              <Typography
+                variant="small"
+              >
+                주소: 서울특별시 노원구 덕릉로 118길 29
+              </Typography>
+              <Typography
+                variant="small"
+              >
+                문의 : asdasfafsf@naver.com
+              </Typography>
+            </div>
+            <div className="mt-6">
+              <Typography
+                variant="small"
+              >
+                CopyRight 2024 AlgoGo Co. all right reserved.
+              </Typography>
+            </div>
           </div>
         </div>
+
       </div>
     </footer>
   );
