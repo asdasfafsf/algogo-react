4389785fe93c26af5bbd17bcc665a47d6334a318
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-05-29 20:34:23 +0900

    feat: add mypage

--- src/page/My.tsx
+++ src/page/My.tsx
@@ -1,28 +1,18 @@
-import ProfilePhoto from '../atom/ProfilePhoto';
+import { Typography } from '@material-tailwind/react';
 import DefaultLayout from '../layout/DefaultLayout';
-import Main from '../template/Main';
+import BasicMyInfo from '../organism/BasicMyInfo';
+import ConnectedInfo from '../organism/ConnectedInfo';
 
 function My() {
   return (
     <DefaultLayout>
-      <div className="min-h-[calc(100vh-300px)] w-full py-4 flex flex-col items-center">
-        <div className="w-full h-full p-8 bg-white rounded-lg shadow-md">
-          <div className="flex justify-start gap-8">
-            <ProfilePhoto />
-            <div className="flex items-center h-full">
-              <div>
-                <div className="w-full h-full mb-4">
-                  <h2 className="text-2xl font-semibold">이름</h2>
-                  <p className="text-gray-600">한원근</p>
-                </div>
-                <div className="w-full h-full mb-4">
-                  <h2 className="text-2xl font-semibold">이메일</h2>
-                  <p className="text-gray-600">asdasfafsf@naver.com</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="min-h-[calc(100vh-300px)] w-full py-4 flex flex-col">
+        <Typography variant="h4" className="my-6">기본 정보</Typography>
+        <BasicMyInfo />
+
+        <Typography variant="h4" className="my-6">연동 정보</Typography>
+        <ConnectedInfo />
+
       </div>
     </DefaultLayout>
 
