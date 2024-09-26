f4a1af00aae95d240f4f7842d2c253c5568a82ac
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-09-26 19:11:32 +0900

    feat: 로그인, 회원가입 통합

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -3,17 +3,25 @@ import {
 } from '@material-tailwind/react';
 import { useNavigate } from 'react-router-dom';
 
-export default function Login() {
+const { VITE_ENV } = import.meta.env;
+
+interface LoginProps {
+  name?: '로그인' | '회원가입'
+}
+
+export default function Login({ name = '로그인' }: LoginProps) {
   const navigate = useNavigate();
   const handleOAuth = async (_e: React.MouseEvent<HTMLButtonElement>, provider: 'google' | 'kakao' | 'github') => {
-    const url = `http://localhost:3001/v1/oauth/${provider}`;
+    const url = VITE_ENV === 'development'
+      ? `http://localhost:3001/v1/oauth/${provider}`
+      : `https://www.algogo.co.kr/v1/oauth${provider}`;
     window.location.href = url;
   };
   return (
     <section className="grid items-center h-screen p-8">
       <div className="text-center">
         <Typography variant="h3" color="blue-gray" className="mb-2">
-          로그인
+          {name}
         </Typography>
         {/* <Typography color="gray" className="mb-12 font-normal" /> */}
         <form action="#" className="mx-auto max-w-[24rem] text-left">
@@ -64,10 +72,10 @@ export default function Login() {
             color="gray"
             className="mt-6 font-normal text-center"
           >
-            회원이 아니신가요?
+            {name === '로그인' ? '회원이 아니신가요?' : '회원이신가요?'}
             {' '}
-            <a href="/signup" className="font-medium text-gray-900">
-              회원가입
+            <a href={name === '로그인' ? '/signup' : '/login'} className="font-medium text-gray-900">
+              {name === '로그인' ? '회원가입' : '로그인'}
             </a>
           </Typography>
           <div className="flex items-center w-full gap-2 my-6">
