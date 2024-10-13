1e2c28cf6cb29fb5d04b15a92f381d456d02df34
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-10-13 23:25:09 +0900

    fix: inputLust=> inputList

--- src/organism/CodeResultPannel.tsx
+++ src/organism/CodeResultPannel.tsx
@@ -55,7 +55,7 @@ export default function CodeResultPannel() {
                 seq: 0,
                 provider: language,
                 code,
-                inputLust: [{ seq: '1', input }],
+                inputList: [{ seq: '1', input }],
               });
             }}
             handleClickPaste={async () => {
