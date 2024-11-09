3f5a06d9f6f26ca8844f265b552e8c392a938118
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-11-09 15:41:11 +0900

    feat:

--- src/components/Table/ProblemTable.tsx
+++ src/components/Table/ProblemTable.tsx
@@ -1,51 +1,87 @@
 import { Card } from '@components/Card/index';
 import { useState } from 'react';
-import ProblemStateChip from '../Chip/ProblemStateChip';
+import { ProblemStateChip, ProblemLevelChip } from '@components/Chip/index';
 import Typography from '../Typography/Typography';
+import Button from '../Button/Button';
+import ProblemThSort from './ProblemThSort';
+import {
+  PROBLEM_SORT_ANSWER_RATE_ASC,
+  PROBLEM_SORT_ANSWER_RATE_DESC,
+  PROBLEM_SORT_LEVEL_ASC, PROBLEM_SORT_LEVEL_DESC, PROBLEM_SORT_TITLE_ASC, PROBLEM_SORT_TITLE_DESC,
+} from '../../constant/ProblemSort';
 
 export default function ProblemTable() {
-  const [problemSort, setProblemSort] = useState(0);
+  const [problemSort, setProblemSort] = useState<ProblemSort>(0);
   const [isOpenProblemGrade, setProblemOpenGrade] = useState(false);
 
   return (
-    <Card>
+    <Card className="p-0 overflow-x-auto">
       반가워
-      <div className="overflow-x-auto">
-        <table className="w-full min-w-[800px] table-fixed">
-          <thead>
-            <tr>
-              <th className="w-24 pl-2.5 text-left">
-                <Typography weight="semibold" variant="medium">상태</Typography>
-              </th>
-              <th className="w-full text-left min-w-24">
-                <Typography weight="semibold"variant="medium">제목</Typography>
-              </th>
-              <th className="w-24 text-left">
-                <Typography weight="semibold"variant="medium">난이도</Typography>
-              </th>
-              <th className="w-24 text-left">
-                <Typography weight="semibold" variant="medium">정답률</Typography>
-              </th>
-              <th className="w-24 text-left">
-                <Typography weight="semibold" variant="medium">제출</Typography>
-              </th>
-              <th className="w-24 text-left">
-                <Typography weight="semibold" variant="medium">출처</Typography>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td><ProblemStateChip state={0} value="" /></td>
-              <td>테dd스트 제목</td>
-              <td>상</td>
-              <td>90%</td>
-              <td>100회</td>
-              <td>사이트</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <Button type="text" size="small" color="blue">방가</Button>
+
+      <table className="w-full min-w-[800px] table-fixed">
+        <thead className="h-12 mb-12 border-b border-gray-300">
+          <tr>
+            <th className="w-16 pl-3 text-center">
+              <Typography weight="semibold" variant="medium">
+                상태
+              </Typography>
+            </th>
+            <ProblemThSort
+              className="w-full pl-12 text-left min-w-24"
+              sort={problemSort === PROBLEM_SORT_TITLE_ASC
+                ? 1 : problemSort === PROBLEM_SORT_TITLE_DESC ? 2 : 0}
+            >
+              제목
+            </ProblemThSort>
+
+            <ProblemThSort
+              className="text-left w-36"
+              sort={problemSort === PROBLEM_SORT_LEVEL_ASC
+                ? 1 : problemSort === PROBLEM_SORT_LEVEL_DESC ? 2 : 0}
+            >
+              난이도
+            </ProblemThSort>
+            <ProblemThSort
+              className="w-32 text-left"
+              sort={problemSort === PROBLEM_SORT_ANSWER_RATE_ASC
+                ? 1 : problemSort === PROBLEM_SORT_ANSWER_RATE_DESC ? 2 : 0}
+            >
+              정답률
+            </ProblemThSort>
+            <ProblemThSort
+              className="w-24 text-left"
+              sort={problemSort === 40 ? 1 : problemSort === 41 ? 2 : 0}
+            >
+              제출
+            </ProblemThSort>
+            <th className="w-24 text-left">
+              <Typography weight="semibold" variant="medium">출처</Typography>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="h-16 border-b border-gray-300">
+            <td className="flex items-center justify-center w-full h-16 pl-3">
+              <ProblemStateChip state={0} value="" />
+            </td>
+            <td className="w-full pl-12"><Typography variant="medium" weight="semilight">저는 문제입니다</Typography></td>
+            <td className="w-full">
+              <div className="flex items-center w-full justify-left">
+                <ProblemLevelChip level="플래티넘 5" />
+              </div>
+            </td>
+            <td>
+              <Typography variant="medium" weight="light">100%</Typography>
+            </td>
+            <td><Typography variant="medium" weight="light">49521312</Typography></td>
+            <td>
+              <Typography variant="medium" weight="light">49521312</Typography>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
     </Card>
   );
 }
