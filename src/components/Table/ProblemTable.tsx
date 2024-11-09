0ac8dcbd99d0dd617e790a358cb23ea1a22bc27e
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-11-09 20:25:11 +0900

    feat: change font

--- src/components/Table/ProblemTable.tsx
+++ src/components/Table/ProblemTable.tsx
@@ -1,6 +1,7 @@
 import { Card } from '@components/Card/index';
 import { useState } from 'react';
 import { ProblemStateChip, ProblemLevelChip } from '@components/Chip/index';
+import { LinkIcon } from '@heroicons/react/24/solid';
 import Typography from '../Typography/Typography';
 import Button from '../Button/Button';
 import ProblemThSort from './ProblemThSort';
@@ -36,51 +37,60 @@ export default function ProblemTable() {
             </ProblemThSort>
 
             <ProblemThSort
-              className="text-center w-36"
+              className="pl-2 text-center w-48"
               sort={problemSort === PROBLEM_SORT_LEVEL_ASC
                 ? 1 : problemSort === PROBLEM_SORT_LEVEL_DESC ? 2 : 0}
             >
               난이도
             </ProblemThSort>
             <ProblemThSort
-              className="w-32"
+              className="w-44"
               sort={problemSort === PROBLEM_SORT_ANSWER_RATE_ASC
                 ? 1 : problemSort === PROBLEM_SORT_ANSWER_RATE_DESC ? 2 : 0}
             >
               정답률
             </ProblemThSort>
             <ProblemThSort
-              className="w-24"
+              className="w-32"
               sort={problemSort === 40 ? 1 : problemSort === 41 ? 2 : 0}
             >
               제출
             </ProblemThSort>
-            <th className="w-24">
-              <Typography weight="semibold" variant="medium">출처</Typography>
+            <th className="w-20">
+              <div className="flex items-center w-full">
+                <Typography className="text-left" weight="semibold" variant="medium">출처</Typography>
+              </div>
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr className="h-16 border-b border-gray-300">
-            <td className="flex items-center justify-center w-full h-16 pl-3">
-              <ProblemStateChip state={0} value="" />
-            </td>
-            <td className="w-full pl-12">
-              <Typography variant="medium" weight="semilight">저는 문제입니다</Typography>
-            </td>
-            <td className="w-full">
-              <div className="flex items-center w-full justify-left">
-                <ProblemLevelChip level="플래티넘 5" />
-              </div>
-            </td>
-            <td>
-              <Typography variant="medium" weight="light">100%</Typography>
-            </td>
-            <td><Typography variant="medium" weight="light">49521312</Typography></td>
-            <td>
-              <Typography variant="medium" weight="light">49521312</Typography>
-            </td>
-          </tr>
+          {['플래티넘 5', '브론즈 1', '다이아 1', '골드 1', '실버 1'].map((elem) => (
+            <tr key={elem} className="h-16 border-b border-gray-300">
+              <td className="flex items-center justify-center w-full h-16 pl-3">
+                <ProblemStateChip state={0} value="" />
+              </td>
+              <td className="w-full pl-12">
+                <Typography className="text-gray-700" variant="medium" weight="semilight">저는 문제입니다</Typography>
+              </td>
+              <td className="w-full">
+                <div className="flex items-center w-full justify-left">
+                  <ProblemLevelChip level={elem} />
+                </div>
+              </td>
+              <td>
+                <Typography className="text-gray-700" variant="medium" weight="semilight">100%</Typography>
+              </td>
+              <td>
+                <Typography className="text-gray-700" variant="medium" weight="semilight">1</Typography>
+              </td>
+              <td>
+                <Typography className="text-gray-700" variant="medium" weight="semilight">
+                  <LinkIcon className="w-4 h-4" />
+                </Typography>
+              </td>
+            </tr>
+          ))}
+
         </tbody>
       </table>
 
