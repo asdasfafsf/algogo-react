04f20473c84724d65ddf481d52f5ed0ede540462
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-11-09 15:40:39 +0900

    feat: add semilight

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 // src/components/Heading.tsx
 type Variant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'paragraph' | 'small' | 'medium';
-type Weight = 'bold' | 'light' | 'semibold';
+type Weight = 'bold' | 'light' | 'semibold' | 'regular' | 'semilight';
 
 export interface HeadingProps {
   children: React.ReactNode;
@@ -16,46 +16,64 @@ const headingClasses: Record<Variant, Record<Weight, string>> = {
     bold: 'font-bold leading-snug tracking-tight text-slate-800 mx-auto my-6 w-full text-2xl',
     light: 'font-light leading-snug tracking-tight text-slate-800 mx-auto my-6 w-full text-2xl',
     semibold: 'font-semibold leading-snug tracking-tight text-slate-800 mx-auto my-6 w-full text-2xl',
+    regular: 'font-medium leading-snug tracking-tight text-slate-800 mx-auto my-6 w-full text-2xl',
+    semilight: 'font-normal leading-snug tracking-tight text-slate-800 mx-auto my-6 w-full text-2xl',
   },
   h2: {
     bold: 'font-bold leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-2xl',
     light: 'font-light leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-2xl',
     semibold: 'font-semibold leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-2xl',
+    regular: 'font-medium leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-2xl',
+    semilight: 'font-normal leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-2xl',
   },
   h3: {
     bold: 'font-bold leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-xl max-w-lg',
     light: 'font-light leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-xl max-w-lg',
     semibold: 'font-semibold leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-xl max-w-lg',
+    regular: 'font-medium leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-xl max-w-lg',
+    semilight: 'font-normal leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-xl max-w-lg',
   },
   h4: {
     bold: 'font-bold leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-lg max-w-md',
     light: 'font-light leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-lg max-w-md',
     semibold: 'font-semibold leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-lg max-w-md',
+    regular: 'font-medium leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-lg max-w-md',
+    semilight: 'font-normal leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-lg max-w-md',
   },
   h5: {
     bold: 'font-bold leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-base max-w-sm',
     light: 'font-light leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-base max-w-sm',
     semibold: 'font-semibold leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-base max-w-sm',
+    regular: 'font-medium leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-base max-w-sm',
+    semilight: 'font-normal leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-base max-w-sm',
   },
   h6: {
     bold: 'font-bold leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-sm max-w-xs',
     light: 'font-light leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-sm max-w-xs',
     semibold: 'font-semibold leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-sm max-w-xs',
+    regular: 'font-medium leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-sm max-w-xs',
+    semilight: 'font-normal leading-snug tracking-normal text-slate-800 mx-auto my-6 w-full text-sm max-w-xs',
   },
   paragraph: {
     bold: 'font-bold leading-relaxed tracking-normal text-slate-800 text-base',
     light: 'font-light leading-relaxed tracking-normal text-slate-800 text-base',
     semibold: 'font-semibold leading-relaxed tracking-normal text-slate-800 text-base',
+    regular: 'font-medium leading-relaxed tracking-normal text-slate-800 text-base',
+    semilight: 'font-normal leading-relaxed tracking-normal text-slate-800 text-base',
   },
   medium: {
     bold: 'font-bold leading-snug tracking-normal text-slate-800 text-sm',
     light: 'font-light leading-snug tracking-normal text-slate-800 text-sm',
     semibold: 'font-semibold leading-snug tracking-normal text-slate-800 text-sm',
+    regular: 'font-medium leading-snug tracking-normal text-slate-800 text-sm',
+    semilight: 'font-normal leading-snug tracking-normal text-slate-800 text-sm',
   },
   small: {
     bold: 'font-bold leading-snug tracking-normal text-slate-800 text-xs',
     light: 'font-light leading-snug tracking-normal text-slate-800 text-xs',
     semibold: 'font-semibold leading-snug tracking-normal text-slate-800 text-xs',
+    regular: 'font-medium leading-snug tracking-normal text-slate-800 text-xs',
+    semilight: 'font-normal leading-snug tracking-normal text-slate-800 text-xs',
   },
 };
 
@@ -69,7 +87,7 @@ export default function Typography({
   const classes = headingClasses[variant][weight] || headingClasses.h1.bold;
 
   return (
-    <Tag className={`${className} ${classes} animate-fadeIn`}>
+    <Tag className={`${className} ${classes}`}>
       {children}
     </Tag>
   );
