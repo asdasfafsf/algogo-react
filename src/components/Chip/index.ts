fdcce334e4d563cd3c0ebf9eedf75703f7a5c0b4
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-11-14 19:50:10 +0900

    fix: fix chip index

--- src/components/Chip/index.ts
+++ src/components/Chip/index.ts
@@ -1,8 +1,10 @@
 import Chip from './Chip';
 import ProblemStateChip from './ProblemStateChip';
 import ProblemLevelChip from './ProvlemLevelChip';
+import ChipWithSelected from './ChipWithSelected';
 
 export {
+  ChipWithSelected,
   ProblemLevelChip,
   ProblemStateChip,
   Chip,
