7d17453228c7c755a99854906a5b527e1853e85e
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-11-14 20:26:40 +0900

    feat: fix ProblemTable

--- src/components/Table/ProblemTable.tsx
+++ src/components/Table/ProblemTable.tsx
@@ -1,16 +1,20 @@
 import { Card } from '@components/Card/index';
 import { useState } from 'react';
 import { ProblemStateChip, ProblemLevelChip } from '@components/Chip/index';
-import { LinkIcon } from '@heroicons/react/24/solid';
+import { LinkIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 import { Pagebar } from '@components/Pagebar/index';
 import { Input } from '@components/Input/index';
-import Typography from '../Typography/Typography';
-import Button from '../Button/Button';
+import { ProblemTypeDropdown, ProblemLevelDropdown, ProblemStateDropdown } from '@components/Dropdown/index';
+import { Button } from '@components/Button/index';
+import { Typography } from '@components/Typography/index';
 import ProblemThSort from './ProblemThSort';
 import {
   PROBLEM_SORT_ANSWER_RATE_ASC,
   PROBLEM_SORT_ANSWER_RATE_DESC,
-  PROBLEM_SORT_LEVEL_ASC, PROBLEM_SORT_LEVEL_DESC, PROBLEM_SORT_TITLE_ASC, PROBLEM_SORT_TITLE_DESC,
+  PROBLEM_SORT_LEVEL_ASC,
+  PROBLEM_SORT_LEVEL_DESC,
+  PROBLEM_SORT_TITLE_ASC,
+  PROBLEM_SORT_TITLE_DESC,
 } from '../../constant/ProblemSort';
 
 export default function ProblemTable() {
@@ -19,12 +23,37 @@ export default function ProblemTable() {
 
   return (
     <Card className="p-0">
-      반가워
-      <Button variant="text" size="small" color="blue">방가</Button>
+      <div
+        className="flex flex-wrap justify-between gap-4 p-6 mb-4 rounded-none"
+      >
+        <div className="w-full">
+          <div className="w-full h-12">
+            <Typography variant="h5">
+              모든 문제
+            </Typography>
+          </div>
+          <div className="flex flex-wrap items-center justify-between w-full gap-2">
+            <div className="flex items-center gap-2">
+              <ProblemTypeDropdown />
+              <ProblemLevelDropdown />
+              <ProblemStateDropdown />
+            </div>
+            <div className="flex flex-wrap items-center justify-end w-full gap-4 shrink-0 md:w-max">
+              {/* <div className="md:w-72"> */}
+              <Input
+                label="제목"
+                icon={<MagnifyingGlassIcon className="w-5 h-5" />}
+              />
+              {/* </div> */}
+              <Button
+                className="w-full md:max-w-fit"
+              >
+                검색
+              </Button>
+            </div>
+          </div>
 
-      <div>
-        방가방
-        <Input label="제목" />
+        </div>
       </div>
 
       <div className="overflow-x-auto">
