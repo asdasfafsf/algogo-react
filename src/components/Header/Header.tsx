c40b29d078efd54b5992da3b54653d090e08d740
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-11-16 21:41:18 +0900

    feat: add Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,84 @@
+import {
+  Typography,
+  Button,
+} from '@material-tailwind/react';
+
+import { useNavigate } from 'react-router-dom';
+import { Navbar, NavbarItem, NavbarSubItem } from '@components/Navbar/index';
+import { Line } from '@components/Line/index';
+import useMeStore from '@zustand/MeStore';
+
+import ProfileMenu from '../molecule/ProfileMenu';
+
+export default function Header() {
+  const navigate = useNavigate();
+  const meStore = useMeStore(({ me }) => ({ me }));
+  const { me } = meStore;
+
+  return (
+    <header className="w-screen">
+      <div className="flex items-center justify-center pt-4">
+        <div className="container flex items-center justify-between h-12 p-4">
+          <div className="flex items-center gap-1">
+            {/* <img
+                className="w-12 h-12"
+                alt="Logo"
+                src="/Logo.webp"
+              /> */}
+            <Typography
+              className="relative top-1 font-WavvePADO-Regular"
+              variant="h2"
+            >
+              <a href="/">
+                알고고
+              </a>
+            </Typography>
+          </div>
+          <div className="flex items-center h-full gap">
+            {
+                me === null ? (
+                  <>
+                    <Button
+                      variant="text"
+                      className="text-sm font-medium"
+                      onClick={() => navigate('/login')}
+                    >
+                      로그인
+                    </Button>
+                    <Button
+                      variant="text"
+                      className="text-sm font-medium"
+                      onClick={() => navigate('/signup')}
+                    >
+                      회원가입
+                    </Button>
+                  </>
+                )
+                  : <ProfileMenu me={me as Me} />
+              }
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center">
+        <Navbar>
+          <NavbarItem
+            onClick={() => {}}
+            value="문제"
+            isSelected
+          >
+            {location.pathname === '/'
+              ? (
+                <>
+                  <NavbarSubItem isSelected value="모든 문제" to="/" />
+                  <NavbarSubItem isSelected={false} value="출제하기" to="/" />
+                </>
+              )
+              : ''}
+
+          </NavbarItem>
+        </Navbar>
+      </div>
+      <Line />
+    </header>
+  );
+}
