bafa7e7b1658b4df92117e41b9c2d288bb05fd59
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-11-25 20:40:28 +0900

    feat: add pagebar hook

--- src/components/Problem/ProblemListCardFooter.tsx
+++ src/components/Problem/ProblemListCardFooter.tsx
@@ -1,13 +1,19 @@
 import { Pagebar } from '@components/Pagebar/index';
+import useProblemListPagebar from '@hook/problem/useProblemListPagebar';
 
 export default function ProblemListCardFooter() {
+  const {
+    pagingInfo,
+    maxPageNo,
+    handleChangePageNo,
+  } = useProblemListPagebar();
   return (
     <div className="flex items-center w-full h-20">
       <Pagebar
-        currentPage={1}
+        currentPage={pagingInfo.pageNo}
         displayedPageRange={10}
-        maxPage={10}
-        handleChangePage={() => {}}
+        maxPage={maxPageNo}
+        handleChangePage={handleChangePageNo}
       />
     </div>
   );
