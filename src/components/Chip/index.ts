fe04c917975adbe23ca1d6be31caee74cff3f530
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-12-01 22:17:27 +0900

    refactor: remove unused

--- src/components/Chip/index.ts
+++ src/components/Chip/index.ts
@@ -2,8 +2,10 @@ import Chip from './Chip';
 import ProblemStateChip from './ProblemStateChip';
 import ProblemLevelChip from './ProvlemLevelChip';
 import ChipWithSelected from './ChipWithSelected';
+import ProblemCategoryChip from './ProblemCategoryChip';
 
 export {
+  ProblemCategoryChip,
   ChipWithSelected,
   ProblemLevelChip,
   ProblemStateChip,
