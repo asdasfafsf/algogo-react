4026758a0be35e3e547b45829deb10e4da85cd2f
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-12-02 16:55:27 +0900

    feat: update tooltip

--- src/components/common/Tooltip.tsx
+++ src/components/common/Tooltip.tsx
@@ -5,7 +5,19 @@ import 'react-tooltip/dist/react-tooltip.css';
 interface WrappedTooltipProps {
   children: React.ReactElement;
   content: string;
-  placement?: 'top' | 'bottom' | 'left' | 'right';
+  placement?:
+  | 'top'
+  | 'top-start'
+  | 'top-end'
+  | 'bottom'
+  | 'bottom-start'
+  | 'bottom-end'
+  | 'left'
+  | 'left-start'
+  | 'left-end'
+  | 'right'
+  | 'right-start'
+  | 'right-end';
 }
 
 function Tooltip({
