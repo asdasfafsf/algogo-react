c96a500b16e2f0e504958da3a68549ffe1a779e3
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-12-26 20:07:55 +0900

    fix: remove unused

--- src/components/Line/Line.tsx
+++
@@ -1,8 +0,0 @@
-interface LineProps {
-  className?: string;
-}
-
-export default function Line({ className }: LineProps) {
-  const defaultClassName = `${className ?? ''} h-px w-full bg-gray-300`.trim();
-  return <div className={defaultClassName} />;
-}
--- src/components/Line/index.ts
+++
@@ -1,5 +0,0 @@
-import Line from './Line';
-
-export {
-  Line,
-};
