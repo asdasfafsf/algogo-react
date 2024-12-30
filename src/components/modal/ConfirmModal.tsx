fffe10de6b6ff6422c1255596df656a3a0472f1d
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-12-30 17:19:05 +0900

    fix: fix cancelbtn callback function ( handleOk to handleCancel)

--- src/components/modal/ConfirmModal.tsx
+++ src/components/modal/ConfirmModal.tsx
@@ -80,7 +80,7 @@ export default function ComfirmModal({ content }: AlertModalProps) {
           </Button>
           <Button
             size="small"
-            onClick={handleOk}
+            onClick={handleCancel}
             className="bg-gray-500"
             color="gray"
           >
