117f32180a3efeba6c0b09fef90366d118675d56
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-01-07 12:52:51 +0900

    refactor: add eslint-disable use any

--- src/plugins/modal/ModalContainer.tsx
+++ src/plugins/modal/ModalContainer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/jsx-no-useless-fragment */
 import { useEffect } from 'react';
