a2b918814c00f4e05e35f31e263bc7ac60d1b4e2
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-01-16 19:49:21 +0900

    feat: add refresh

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -4,33 +4,75 @@ const { VITE_ENV } = import.meta.env;
 const baseURL = VITE_ENV === 'development' ? 'http://localhost:3001' : 'https://www.algogo.co.kr';
 const headers = { 'Content-Type': 'application/json;charset=UTF-8' };
 const withCredentials = VITE_ENV === 'development';
+
 const apiClient = axios.create({
   baseURL,
   headers,
   withCredentials,
 });
 
+let isRefreshing = false;
+const failedQueue: {
+  resolve: (value?: unknown) => void;
+  reject: (reason?: ApiResponse<unknown>) => void;
+}[] = [];
+
 apiClient.interceptors.request.use((config) => {
   const token = localStorage.getItem('accessToken');
-
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
 
-// 응답 인터셉터 설정
 apiClient.interceptors.response.use(
   (response) => response,
-  (error) => {
-    if (error.response) {
-      if (error.statusCode === 404) {
+  async (error) => {
+    const { response: { data } = {}, config } = error;
 
+    if (data?.statusCode === 401 && data?.errorMessage.includes('만료')) {
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        })
+          .then(() => apiClient.request(config))
+          .catch((err) => Promise.reject(err));
       }
 
-      return error.response;
+      isRefreshing = true;
+
+      const refreshToken = localStorage.getItem('refreshToken');
+      if (!refreshToken) {
+        failedQueue.forEach(({ reject }) => reject(error));
+        failedQueue.length = 0;
+        isRefreshing = false;
+        return Promise.resolve({
+          status: 401,
+          errorCode: 'UNAUTHORIZED',
+          errorMessage: 'refreshToken이 없습니다.',
+          data: null,
+        });
+      }
+
+      try {
+        const { data: refreshResponse } = await apiClient.post('/api/v1/auth/refresh', { refreshToken });
+        localStorage.setItem('accessToken', refreshResponse.data.accessToken);
+        localStorage.setItem('refreshToken', refreshResponse.data.refreshToken);
+
+        failedQueue.forEach(({ resolve }) => resolve());
+        failedQueue.length = 0;
+
+        return await apiClient.request(config);
+      } catch (refreshError) {
+        failedQueue.forEach(({ reject }) => reject());
+        failedQueue.length = 0;
+        return await Promise.reject(refreshError);
+      } finally {
+        isRefreshing = false;
+      }
     }
-    return { statusCode: error.statusCode, errorCode: '9999', errorMessage: '정의되지 않은 오류가 발생하였습니다.' };
+
+    return Promise.reject(error);
   },
 );
 
--- src/api/auth.ts
+++ src/api/auth.ts
@@ -5,3 +5,8 @@ export const getToken = async (): Promise<ApiResponse<Token>> => {
   const response: AxiosResponse<ApiResponse<Token>> = await apiClient.get('/api/v1/auth/token');
   return response.data;
 };
+
+export const refreshToken = async (): Promise<ApiResponse<Token>> => {
+  const response: AxiosResponse<ApiResponse<Token>> = await apiClient.post('/api/v1/auth/refresh');
+  return response.data;
+};
