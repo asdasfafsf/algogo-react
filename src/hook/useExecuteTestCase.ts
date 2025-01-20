6f3e6dcb10ef183cbfbf1117f838330ed65992c3
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-01-20 20:29:30 +0900

    feat: add refresh for testcase

--- src/hook/useExecuteTestCase.ts
+++ src/hook/useExecuteTestCase.ts
@@ -1,4 +1,5 @@
 import { useCallback } from 'react';
+import useMeStore from '@zustand/MeStore';
 import useCodeEditorStore from '../zustand/CodeEditorStore';
 import useTestCaseListStore from '../zustand/TestCaseListStore';
 import { useExecuteSocketStore } from '../zustand/ExecuteSocketStore';
@@ -14,7 +15,7 @@ export default function useExecuteTestCase() {
   const state = useExecuteSocketStore((state) => state.state);
 
   const setSelectedIndex = useCodeResultPanelStore((state) => state.setSelectedIndex);
-
+  const refresh = useMeStore((state) => state.refresh);
   const [alert] = useAlertModal();
   const modal = useModal();
 
@@ -25,10 +26,16 @@ export default function useExecuteTestCase() {
       return;
     }
 
-    if (state === 'DISCONNECTED') {
-      await connect();
+    let currentState:SocketState = state;
+
+    if (currentState === 'DISCONNECTED') {
+      currentState = await connect();
     }
 
+    if (currentState === 'JWT_EXPIRED') {
+      await refresh();
+      currentState = await connect();
+    }
     const { run, execute } = useExecuteSocketStore.getState();
 
     if (modal?.top()?.key === 'TESTCASE') {
@@ -40,6 +47,7 @@ export default function useExecuteTestCase() {
 
     setSelectedIndex(2);
     setRunning();
+    execute(handleExecute);
     const data: RequestExecuteList = {
       code,
       provider: language,
@@ -48,8 +56,23 @@ export default function useExecuteTestCase() {
         input: testCase.input,
       })),
     };
-    execute(handleExecute);
-    run(data, handleRun);
+
+    let result = await run(data);
+
+    if (result.code === 'JWT_EXPIRED') {
+      await refresh();
+      currentState = await connect();
+
+      if (currentState === 'WAITING') {
+        setRunning();
+        execute(handleExecute);
+        result = await run(data);
+      }
+    }
+
+    if (result.code === '9002') {
+      handleRun(result);
+    }
   }, []);
 
   return {
