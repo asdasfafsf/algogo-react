1df01f5a0ae12fa3a0655a46bff45a3d2ad80435
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-02-15 15:33:18 +0900

    feat: add setting api

--- src/api/code.ts
+++ src/api/code.ts
@@ -10,3 +10,13 @@ export const loadCode = async (problemUuid: string, language: Language) => {
   const response: AxiosResponse<ApiResponse<ResponseCode>> = await apiClient.get(`/api/v1/code/problem/${problemUuid}?language=${encodeURIComponent(language)}`);
   return response.data;
 };
+
+export const getSetting = async () => {
+  const response: AxiosResponse<ApiResponse<ResponseSetting>> = await apiClient.get('/api/v1/code/setting');
+  return response.data;
+};
+
+export const setSetting = async (setting: RequestSetting) => {
+  const response: AxiosResponse<ApiResponse<null>> = await apiClient.put('/api/v1/code/setting', setting);
+  return response.data;
+};
--- src/type/Code.d.ts
+++ src/type/Code.d.ts
@@ -7,3 +7,21 @@ type RequestSaveCode = {
 type ResponseCode = {
   content: string;
 };
+
+type ResponseSetting = {
+  fontSize: number;
+  problemContentRate: number;
+  theme: 'vs-dark' | 'light'
+  tabSize: number;
+  lineNumber: 'on' | 'off' | 'relative',
+  defaultLanguage: Language
+};
+
+type RequestSetting = {
+  fontSize?: number;
+  problemContentRate?: number;
+  theme?: 'vs-dark' | 'light';
+  tabSize?: number;
+  lineNumber?: 'on' | 'off' | 'relative',
+  defaultLanguage?: Language
+};
