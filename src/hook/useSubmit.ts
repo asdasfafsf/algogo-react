62a9ba4a320628a32bb57d6c4135433b4b45b177
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-02-17 00:10:35 +0900

    feat: 제출누르면 제출페이지로 이동함

--- src/hook/useSubmit.ts
+++ src/hook/useSubmit.ts
@@ -1,9 +1,17 @@
 import { useCallback } from 'react';
+import useProblemStore from '@zustand/ProblemStore';
 
 export default function useSubmit() {
+  const problem = useProblemStore((state) => state.problem);
   const handleSubmit = useCallback(async () => {
-    const uuid = location.pathname.split('/')[2];
-  }, []);
+    if (problem) {
+      const { sourceId, source } = problem;
+
+      if (source === 'BOJ') {
+        window.open(`https://www.acmicpc.net/submit/${sourceId}`, '_blank');
+      }
+    }
+  }, [problem]);
 
   return { handleSubmit };
 }
