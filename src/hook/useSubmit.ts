31cc30345b75b19ad26176bffd1f155d62ad6a00
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-02-17 00:14:43 +0900

    feat: 복사추가

--- src/hook/useSubmit.ts
+++ src/hook/useSubmit.ts
@@ -1,17 +1,26 @@
 import { useCallback } from 'react';
 import useProblemStore from '@zustand/ProblemStore';
+import useCodeEditorStore from '@zustand/CodeEditorStore';
 
 export default function useSubmit() {
   const problem = useProblemStore((state) => state.problem);
+  const code = useCodeEditorStore((state) => state.code);
   const handleSubmit = useCallback(async () => {
     if (problem) {
       const { sourceId, source } = problem;
+
+      // Copy code to clipboard
+      try {
+        await navigator.clipboard.writeText(code);
+      } catch {
+        // Do nothing
+      }
 
       if (source === 'BOJ') {
         window.open(`https://www.acmicpc.net/submit/${sourceId}`, '_blank');
       }
     }
-  }, [problem]);
+  }, [problem, code]);
 
   return { handleSubmit };
 }
