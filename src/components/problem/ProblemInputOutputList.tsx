0fd4c86bfd5b1be8d2514e166cf04b3f8d916c92
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-02-22 22:30:57 +0900

    style: 원복

--- src/components/problem/ProblemInputOutputList.tsx
+++ src/components/problem/ProblemInputOutputList.tsx
@@ -44,7 +44,7 @@ export function ProblemInputOutputList({ inputOutputList }: ProblemInputOutputPr
 
       {inputOutputList.map((elem, index) => (
         <div key={`example-${index}`}>
-          <Typography variant="h5">
+          <Typography variant="h5" className="pt-2 font-bold">
             예시
             {index + 1}
           </Typography>
