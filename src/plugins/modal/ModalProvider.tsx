6a0e628a7604ffbc44c1cb6edb3c98fac9173528
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-05-29 21:11:48 +0900

    feat: refreshtoken 만료 시 alert과 함께 리다이렉트 추가

--- src/plugins/modal/ModalProvider.tsx
+++ src/plugins/modal/ModalProvider.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-redeclare */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { AlertModal } from '@components/modal';
 import ModalController from './ModalController';
 import ModalContext from './ModalContext';
 import ModalContainer from './ModalContainer';
@@ -8,9 +9,16 @@ interface ModalProvider {
   children: React.ReactNode
 }
 
+// eslint-disable-next-line import/no-mutable-exports
+export let showAlert: undefined | ((content: string) => Promise<unknown>);
+
 export default function ModalProvider({ children }: ModalProvider) {
   const flagState = useState(1);
   const [modalController] = useState(() => new ModalController(flagState));
+
+  useEffect(() => {
+    showAlert = async (content: string) => modalController.push('Alert', AlertModal, { content });
+  }, [modalController]);
 
   return (
     <ModalContext.Provider value={modalController}>
