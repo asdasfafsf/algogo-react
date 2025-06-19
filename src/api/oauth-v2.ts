12f263edb72619567ba9c39a18b6a8f0036ef0e6
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-06-19 21:36:53 +0900

    feat: 연결, 연결해제 api 추가

--- src/api/oauth-v2.ts
+++ src/api/oauth-v2.ts
@@ -2,6 +2,16 @@ import { AxiosResponse } from 'axios';
 import apiClient from './apiClient';
 
 export const oauthLoginV2 = async ({ provider, code }: { provider: string; code: string }) => {
-  const response: AxiosResponse<ApiResponse<{ accessToken: string; refreshToken: string }>> = await apiClient.get(`api/v2/oauth/${provider}?code=${code}`);
+  const response: AxiosResponse<ApiResponse<{ accessToken: string; refreshToken: string }>> = await apiClient.post(`api/v2/oauth/${provider}?code=${code}`);
+  return response.data;
+};
+
+export const oauthConnectV2 = async ({ provider, code }: { provider: string; code: string }) => {
+  const response: AxiosResponse<ApiResponse<{ accessToken: string; refreshToken: string }>> = await apiClient.post(`api/v2/oauth/connect/${provider}?code=${code}`);
+  return response.data;
+};
+
+export const oauthDisconnectV2 = async ({ provider, code }: { provider: string; code: string }) => {
+  const response: AxiosResponse<ApiResponse<{ accessToken: string; refreshToken: string }>> = await apiClient.post(`api/v2/oauth/disconnect/${provider}?code=${code}`);
   return response.data;
 };
