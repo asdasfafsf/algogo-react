d80e65acf1a50b1c0954b392d2558f16234c5472
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-06-19 21:37:04 +0900

    refactor: 불필요 api 제거

--- src/api/auth-v2.ts
+++ src/api/auth-v2.ts
@@ -1,11 +1,6 @@
 import { AxiosResponse } from 'axios';
 import apiClient from './apiClient';
 
-export const getToken = async (): Promise<ApiResponse<Token>> => {
-  const response: AxiosResponse<ApiResponse<Token>> = await apiClient.get('/api/v1/auth/token');
-  return response.data;
-};
-
 export const refresh = async (): Promise<ApiResponse<Token>> => {
   const response: AxiosResponse<ApiResponse<Token>> = await apiClient.post(
     '/api/v2/auth/refresh',
