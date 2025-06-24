61bca4065b7badee695ae450d6a6c7a6a3cc8d5c
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-06-24 13:30:35 +0900

    feat: add state

--- src/components/problem/ProblemTitle.tsx
+++ src/components/problem/ProblemTitle.tsx
@@ -2,21 +2,25 @@ import {
   Typography, Line,
 } from '@components/common/index';
 import React from 'react';
+import { ProblemState } from '@/type/Problem.type';
+import ProblemStateChip from '../Chip/ProblemStateChip';
 
 interface ProblemTitleProps {
   title: string,
   scale?: number
+  state?: ProblemState
 }
-function ProblemTitle({ title, scale = 1 }: ProblemTitleProps) {
+function ProblemTitle({ title, scale = 1, state }: ProblemTitleProps) {
   return (
     <>
-      <div className="flex items-center">
+      <div className="flex items-center gap-3">
         <Typography
           scale={scale}
           variant="h4"
         >
           {title}
         </Typography>
+        {state && <ProblemStateChip state={state} value="" />}
       </div>
       <Line className="my-2" />
     </>
